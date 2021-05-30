d88e20a21502b485e01b9ee170b9fd5c357c3bef
Meryembentalebeljadil <meryembentaleb11@gmail.com>
2021-05-30 15:46:35 +0200

    react-project inspired by a tuto on youtube

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>What are your tasks of the day!</h1>
+      <h2>By Meryem Bentaleb El Jadil using ReactJS</h2>
+    </div>
+  )
+}
+
+export default Header
